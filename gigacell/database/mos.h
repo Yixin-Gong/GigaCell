63ca330c28a64a7fd1f1dc0b2b8f722e4393fec4
linjingm <1021156572@qq.com>
2023-09-18 15:33:33 +0800

    update mos database construction

--- gigacell/database/mos.h
+++ gigacell/database/mos.h
@@ -6,19 +6,26 @@
 class Mos {
  public:
   Mos() = default;
-  int32_t &getLoc() { return loc_; }
+  float &getGLoc() { return gLoc_; }
+  float &getSLoc() {return sLoc_;}
+  float &getDLoc() {return dLoc_;}
   int32_t &getType() { return type_; }
   std::string &getSource() { return source_; }
   std::string &getGate() { return gate_; }
   std::string &getDrain() { return drain_; }
+  std::string &getMosName(){ return mosName_; }
   float &getWidth() { return width_; }
+
  private:
-  int32_t loc_{};        //xi > 0
+  float gLoc_{};        //gate location
+  float sLoc_{};        //source location
+  float dLoc_{};        //drown location
   int32_t type_{};       //0-NMOS 1-PMOS
   std::string source_; //left net
   std::string gate_;   //
   std::string drain_;  //right net
   float width_{};        //width >= 0 nm
+  std::string mosName_;  //mosName
 
 };
 
