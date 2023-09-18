6922abf0ce5ed13226af65ed07c625b27344ab3a
eric <3258859094@qq.com>
2023-09-18 22:30:23 +0800

    Constructed PlaceDB based on vector type

--- gigacell/placement/place_db.cc
+++ gigacell/placement/place_db.cc
@@ -3,3 +3,19 @@
 //
 
 #include "place_db.h"
+void PlaceDB::init() {
+  uint16_t loc_idx = 0;
+  uint16_t mos_idx = 0;
+  for (auto &kNet : db_.nets()) {
+    for (auto &kMos : kNet.second.net2mos()) {
+      v_mos_ids().emplace_back(loc_idx);
+      v_mos_list().emplace_back(kMos.second.second);
+      loc_idx++;
+
+      v_net2mos_ids().emplace_back(mos_idx);
+      v_net2mos().emplace_back(kMos.second.first, kMos.second.second);
+      mos_idx++;
+    }
+    mos_idx = 0;
+  }
+}
--- gigacell/placement/place_db.h
+++ gigacell/placement/place_db.h
@@ -1,11 +1,41 @@
 //
 // Created by eric on 23-9-15.
 //
-
 #ifndef GIGACELL_GIGACELL_PLACEMENT_PLACE_DB_H_
 #define GIGACELL_GIGACELL_PLACEMENT_PLACE_DB_H_
+#include <vector>
+#include "database/database.h"
 
-class place_db {
+typedef uint32_t index;
+
+class PlaceDB {
+ public:
+  //constructor
+  explicit PlaceDB(DataBase &db) : db_(db) {
+    init();
+  }
+  //setter
+  void init();
+
+  //getter
+  std::vector<index> &v_mos_ids() { return v_mos_ids_; }
+  const std::vector<index> &v_mos_ids() const { return v_mos_ids_; }
+  std::vector<index> &v_net2mos_ids() { return v_net2mos_ids_; }
+  const std::vector<index> &v_net2mos_ids() const { return v_net2mos_ids_; }
+  std::vector<Mos> &v_mos_list() { return v_mos_list_; }
+  const std::vector<Mos> &v_mos_list() const { return v_mos_list_; }
+  std::vector<std::pair<std::string, Mos>> &v_net2mos() { return v_net2mos_; }
+  const std::vector<std::pair<std::string, Mos>> &v_net2mos() const { return v_net2mos_; }
+  std::unordered_map<std::string, Mos> &nets() { return nets_; };
+  const std::unordered_map<std::string, Mos> &nets() const { return nets_; };
+
+ private:
+  DataBase &db_;
+  std::vector<index> v_mos_ids_{};
+  std::vector<Mos> v_mos_list_{};
+  std::vector<index> v_net2mos_ids_{};
+  std::vector<std::pair<std::string, Mos>> v_net2mos_{};
+  std::unordered_map<std::string, Mos> nets_{};
 
 };
 
