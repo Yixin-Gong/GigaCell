1361658704519e97bc99154d204544a2a457fbf4
eric <3258859094@qq.com>
2023-09-19 17:25:22 +0800

    Change the stucture of PlaceDB

--- gigacell/placement/place_db.h
+++ gigacell/placement/place_db.h
@@ -4,9 +4,16 @@
 #ifndef GIGACELL_GIGACELL_PLACEMENT_PLACE_DB_H_
 #define GIGACELL_GIGACELL_PLACEMENT_PLACE_DB_H_
 #include <vector>
+#include <unordered_map>
 #include "database/database.h"
 
 typedef uint16_t index;
+
+struct Net {
+  std::string electrode_name;
+  uint16_t type;
+  index idx;
+};
 
 class PlaceDB {
  public:
@@ -16,15 +23,12 @@ class PlaceDB {
   }
   //setter
   void init();
-
   //getter
-
 
  private:
   DataBase &db_;
   std::vector<index> v_nmos_ids_{};
-
-
+  std::unordered_map<std::string, Net> nets_{};
 };
 
 #endif //GIGACELL_GIGACELL_PLACEMENT_PLACE_DB_H_
