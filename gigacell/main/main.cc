1df54768c035747d9eb197ec52d446c5ac6e319b
Lkyaaa <2310127354@qq.com>
2023-09-22 22:17:36 +0800

    Updated main function

--- gigacell/main/main.cc
+++ gigacell/main/main.cc
@@ -4,13 +4,14 @@
 int main(int argc, char *argv[]) {
 
 
-    std::string filename = "/home/lky/case.txt";
-    gigaplace::DataBase db;
-    gigaplace::Parser parser(db, filename);    cmdline::parser a;
+    //std::string filename = "/home/lky/case.txt";
+
+    cmdline::parser a;
     a.add<std::string>("file_name", 'f', "file name", true, "");
     a.parse_check(argc, argv);
-
-//    std::string filename = a.get<std::string>("file_name");
+    std::string filename = a.get<std::string>("file_name");
+    gigaplace::DataBase db;
+    gigaplace::Parser parser(db, filename);
     parser.parse();
     gigaplace::PlaceDB pl_db(db);
     for (auto &item: pl_db.nets()) {
