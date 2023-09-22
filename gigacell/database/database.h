6da12b63453c4114f04f6743b39def2dd13e1b3e
Lkyaaa <2310127354@qq.com>
2023-09-22 22:02:36 +0800

    Add namespace

--- gigacell/database/database.h
+++ gigacell/database/database.h
@@ -8,20 +8,23 @@
 #include <iostream>
 #include <vector>
 #include "mos.h"
-typedef uint16_t index;
-class DataBase {
-  DataBase() = default;
- public:
-  //getter&&setter
-  std::vector<index> &nmos_ids() { return nmos_ids_; }
-  std::vector<index> &pmos_ids() { return pmos_ids_; }
-  std::vector<Mos> &nmos_list() { return nmos_list_; }
-  std::vector<Mos> &pmos_list() { return pmos_list_; }
- private:
-  std::vector<index> nmos_ids_{};
-  std::vector<Mos> nmos_list_{};
-  std::vector<index> pmos_ids_{};
-  std::vector<Mos> pmos_list_{};
-};
+namespace gigaplace {
+    typedef uint16_t index;
+    class DataBase {
+    public:
+        DataBase() = default;
+        //getter&&setter
+        std::vector<index> &nmos_ids() { return nmos_ids_; }
+        std::vector<index> &pmos_ids() { return pmos_ids_; }
+        std::vector<Mos> &nmos_list() { return nmos_list_; }
+        std::vector<Mos> &pmos_list() { return pmos_list_; }
 
+
+    private:
+        std::vector<index> nmos_ids_{};
+        std::vector<Mos> nmos_list_{};
+        std::vector<index> pmos_ids_{};
+        std::vector<Mos> pmos_list_{};
+    };
+}
 #endif //GIGACELL_GIGACELL_DATABASE_DATABASE_H_
--- gigacell/placement/place_db.h
+++ gigacell/placement/place_db.h
@@ -3,32 +3,38 @@
 //
 #ifndef GIGACELL_GIGACELL_PLACEMENT_PLACE_DB_H_
 #define GIGACELL_GIGACELL_PLACEMENT_PLACE_DB_H_
+
 #include <vector>
 #include <unordered_map>
 #include "database/database.h"
 
-typedef uint16_t index;
+namespace gigaplace {
+    typedef uint16_t index;
 
-struct Net2Mos {
-  std::string electrode_name;
-  uint16_t type;
-  index idx;
-};
+    struct Net2Mos {
+        std::string electrode_name;
+        uint16_t type;
+        index idx;
+    };
 
-class PlaceDB {
- public:
-  //constructor
-  explicit PlaceDB(DataBase &db) : db_(db) {
-    init();
-  }
-  //setter
-  void init();
-  //getter
+    class PlaceDB {
+    public:
+        //constructor
+        explicit PlaceDB(DataBase &db) : db_(db) {
+            init();
+        }
 
+        //setter
+        void init();
 
- private:
-  DataBase &db_;
-  std::unordered_map<std::string, std::vector<Net2Mos>> nets_{};
-};
+        //getter
+        std::unordered_map<std::string, std::vector<Net2Mos>> &nets() { return nets_; }
 
+        const std::unordered_map<std::string, std::vector<Net2Mos>> &nets() const { return nets_; }
+
+    private:
+        DataBase &db_;
+        std::unordered_map<std::string, std::vector<Net2Mos>> nets_{};
+    };
+}
 #endif //GIGACELL_GIGACELL_PLACEMENT_PLACE_DB_H_
