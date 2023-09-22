7a339a619e8b2c2e39f7c75a53c91be1064722ee
Lkyaaa <2310127354@qq.com>
2023-09-22 22:00:27 +0800

    Implemented parser

--- gigacell/parser/parser.cc
+++ gigacell/parser/parser.cc
@@ -2,4 +2,81 @@
 // Created by eric on 23-9-15.
 //
 
-#include "parser.h"
+#include "parser/parser.h"
+#include <sstream>
+namespace gigaplace {
+    void Parser::parse() {
+        file_.open(filename_);
+        if (!file_.is_open()) {
+            std::cerr << "Failed to open file" << filename_ << std::endl;
+            return;
+        }
+        Mos mos;
+        std::string subStr;
+        int32_t pmos_idx = 0;
+        int32_t nmos_idx = 0;
+        std::string line;
+        while (std::getline(file_, line)) {
+            std::vector<std::string> tokens = split_(line, ' ');
+            if (tokens[0] == ".SUBCKT")
+                continue;
+            if (tokens[0] == ".ENDS")
+                break;
+            if (tokens[4] == "VDD") {
+                std::string numStr;
+                for (char c: tokens[6]) {
+                    if (isdigit(c) || c == '.')
+                        numStr += c;
+                }
+                float width;
+                std::istringstream iss(numStr);
+                iss >> width;
+                mos.getWidth() = width;
+                subStr = tokens[0].substr(1);
+                mos.getMosName() = subStr;
+                mos.getDrain() = tokens[1];
+                mos.getGate() = tokens[2];
+                mos.getSource() = tokens[3];
+                mos.getType() = 1;
+                //mos.getWidth()=
+                db_.pmos_list().push_back(mos);
+                db_.pmos_ids().push_back(pmos_idx);
+                pmos_idx++;
+
+
+            } else {
+                std::string numStr;
+                for (char c: tokens[6]) {
+                    if (isdigit(c) || c == '.')
+                        numStr += c;
+                }
+                float width;
+                std::istringstream iss(numStr);
+                iss >> width;
+                mos.getWidth() = width;
+                subStr = tokens[0].substr(1);
+                mos.getMosName() = subStr;
+                mos.getDrain() = tokens[1];
+                mos.getGate() = tokens[2];
+                mos.getSource() = tokens[3];
+                mos.getType() = 0;
+                //mos.getWidth()=
+                db_.nmos_list().push_back(mos);
+                db_.nmos_ids().push_back(nmos_idx);
+                nmos_idx++;
+            }
+
+        }
+        file_.close();
+    }
+
+    std::vector<std::string> Parser::split_(const std::string &str, char delimiter) {
+        std::vector<std::string> tokens;
+        std::string token;
+        std::istringstream tokenStream(str);
+        while (getline(tokenStream, token, delimiter)) {
+            tokens.push_back(token);
+        }
+        return tokens;
+    }
+}
--- gigacell/parser/parser.h
+++ gigacell/parser/parser.h
@@ -5,8 +5,23 @@
 #ifndef GIGACELL_GIGACELL_PARSER_PARSER_H_
 #define GIGACELL_GIGACELL_PARSER_PARSER_H_
 
-class parser {
+#include <utility>
+#include "database/database.h"
+#include <fstream>
 
-};
+namespace gigaplace {
+    class Parser {
+    public:
+        Parser(DataBase &db, std::string filename) : db_(db), filename_(std::move(filename)), file_() {}
 
+        void parse();
+
+    private:
+        std::string filename_;
+        DataBase &db_;
+        std::ifstream file_;
+
+        static std::vector<std::string> split_(const std::string &str, char delimiter);
+    };
+}
 #endif //GIGACELL_GIGACELL_PARSER_PARSER_H_
