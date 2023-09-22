460e71ad33461a106cb92540e1806096eaa763a1
Lkyaaa <2310127354@qq.com>
2023-09-22 22:03:09 +0800

    Implemented parse logic and passed the example file

--- gigacell/main/main.cc
+++ gigacell/main/main.cc
@@ -1,5 +1,23 @@
 #include "main.h"
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
+
+int main(int argc, char *argv[]) {
+
+
+    std::string filename = "/home/lky/case.txt";
+    gigaplace::DataBase db;
+    gigaplace::Parser parser(db, filename);    cmdline::parser a;
+    a.add<std::string>("file_name", 'f', "file name", true, "");
+    a.parse_check(argc, argv);
+
+//    std::string filename = a.get<std::string>("file_name");
+    parser.parse();
+    gigaplace::PlaceDB pl_db(db);
+    for (auto &item: pl_db.nets()) {
+        std::cout << item.first << "\t";
+        for (auto &kItem: item.second) {
+            std::cout << kItem.idx << "\t" << kItem.type << "\t" << kItem.electrode_name << "\t" << std::endl;
+        }
+    }
+    return 0;
 }
--- gigacell/main/main.h
+++ gigacell/main/main.h
@@ -1,1 +1,9 @@
-#include <iostream>
+#pragma once
+
+#include "utils/cmdline.h"
+#include "parser/parser.h"
+#include "placement/place_db.h"
+#include "main/gigaplace.h"
+
+
+
