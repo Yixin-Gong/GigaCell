57111cb95aa31f089d0b0d1da4d1501194ad4836
eric <3258859094@qq.com>
2023-09-26 16:02:04 +0800

    Add libkahypar module to partition

---
+++ gigacell/partition/kahypar.h
@@ -0,0 +1,18 @@
+//
+// Created by eric on 23-9-25.
+//
+
+#ifndef GIGACELL_GIGACELL_PARTITION_KAHYPAR_H_
+#define GIGACELL_GIGACELL_PARTITION_KAHYPAR_H_
+#include<memory>
+#include<vector>
+#include<iostream>
+#include<libkahypar.h>
+namespace gigaplace {
+class KaHyPar {
+ public:
+  void partition();
+
+};
+}
+#endif //GIGACELL_GIGACELL_PARTITION_KAHYPAR_H_
