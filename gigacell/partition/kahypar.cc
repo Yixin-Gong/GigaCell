6330b61e5a0a6707e34db74f0a94ca95a6c2db9a
eric <3258859094@qq.com>
2023-09-30 12:46:08 +0800

    Extract key value from kahypar and add it to the class KaHyPar.

--- gigacell/partition/kahypar.cc
+++ gigacell/partition/kahypar.cc
@@ -4,62 +4,60 @@
 
 #include "kahypar.h"
 void gigaplace::KaHyPar::partition() {
-  kahypar_context_t *context = kahypar_context_new();
-  kahypar_configure_context_from_file(context, "/path/to/config.ini");
+//  context_ = kahypar_context_new();
+  kahypar_configure_context_from_file(context_, "/path/to/config.ini");
+  kahypar_set_seed(context_, 42);
 
-  kahypar_set_seed(context, 42);
-
-  const kahypar_hypernode_id_t num_vertices = 7;
-  const kahypar_hyperedge_id_t num_hyperedges = 4;
-
-  std::unique_ptr<kahypar_hyperedge_weight_t[]> hyperedge_weights = std::make_unique<kahypar_hyperedge_weight_t[]>(4);
+//  const kahypar_hypernode_id_t num_vertices = 7;
+//  const kahypar_hyperedge_id_t num_hyperedges = 4;
+//  std::unique_ptr<kahypar_hyperedge_weight_t[]> hyperedge_weights = std::make_unique<kahypar_hyperedge_weight_t[]>(4);
 
   // force the cut to contain hyperedge 0 and 2
-  hyperedge_weights[0] = 1;
-  hyperedge_weights[1] = 1000;
-  hyperedge_weights[2] = 1;
-  hyperedge_weights[3] = 1000;
+  hyperedge_weights_[0] = 1;
+  hyperedge_weights_[1] = 1000;
+  hyperedge_weights_[2] = 1;
+  hyperedge_weights_[3] = 1000;
 
-  std::unique_ptr<size_t[]> hyperedge_indices = std::make_unique<size_t[]>(5);
+//  std::unique_ptr<size_t[]> hyperedge_indices = std::make_unique<size_t[]>(5);
 
-  hyperedge_indices[0] = 0;
-  hyperedge_indices[1] = 2;
-  hyperedge_indices[2] = 6;
-  hyperedge_indices[3] = 9;
-  hyperedge_indices[4] = 12;
+  hyperedge_indices_[0] = 0;
+  hyperedge_indices_[1] = 2;
+  hyperedge_indices_[2] = 6;
+  hyperedge_indices_[3] = 9;
+  hyperedge_indices_[4] = 12;
 
-  std::unique_ptr<kahypar_hyperedge_id_t[]> hyperedges = std::make_unique<kahypar_hyperedge_id_t[]>(12);
+//  std::unique_ptr<kahypar_hyperedge_id_t[]> hyperedges = std::make_unique<kahypar_hyperedge_id_t[]>(12);
 
   // hypergraph from hMetis manual page 14
-  hyperedges[0] = 0;
-  hyperedges[1] = 2;
-  hyperedges[2] = 0;
-  hyperedges[3] = 1;
-  hyperedges[4] = 3;
-  hyperedges[5] = 4;
-  hyperedges[6] = 3;
-  hyperedges[7] = 4;
-  hyperedges[8] = 6;
-  hyperedges[9] = 2;
-  hyperedges[10] = 5;
-  hyperedges[11] = 6;
+  hyperedges_[0] = 0;
+  hyperedges_[1] = 2;
+  hyperedges_[2] = 0;
+  hyperedges_[3] = 1;
+  hyperedges_[4] = 3;
+  hyperedges_[5] = 4;
+  hyperedges_[6] = 3;
+  hyperedges_[7] = 4;
+  hyperedges_[8] = 6;
+  hyperedges_[9] = 2;
+  hyperedges_[10] = 5;
+  hyperedges_[11] = 6;
 
-  const double imbalance = 0.03;
-  const kahypar_partition_id_t k = 2;
+//  const double imbalance = 0.03;
+//  const kahypar_partition_id_t k = 2;
 
-  kahypar_hyperedge_weight_t objective = 0;
+//  kahypar_hyperedge_weight_t objective = 0;
 
-  std::vector<kahypar_partition_id_t> partition(num_vertices, -1);
+//  std::vector<kahypar_partition_id_t> partition_(num_vertices_, -1);
 
-  kahypar_partition(num_vertices, num_hyperedges,
-                    imbalance, k,
-      /*vertex_weights */ nullptr, hyperedge_weights.get(),
-                    hyperedge_indices.get(), hyperedges.get(),
-                    &objective, context, partition.data());
+  kahypar_partition(num_vertices_, num_hyperedges_,
+                    imbalance_, k_,
+      /*vertex_weights */ nullptr, hyperedge_weights_.get(),
+                    hyperedge_indices_.get(), hyperedges_.get(),
+                    &objective_, context_, partition_.data());
 
-  for (int i = 0; i != num_vertices; ++i) {
-    std::cout << i << ":" << partition[i] << std::endl;
+  for (int i = 0; i != num_vertices_; ++i) {
+    std::cout << i << ":" << partition_[i] << std::endl;
   }
 
-  kahypar_context_free(context);
+  kahypar_context_free(context_);
 }
--- gigacell/partition/kahypar.h
+++ gigacell/partition/kahypar.h
@@ -8,11 +8,35 @@
 #include<vector>
 #include<iostream>
 #include<libkahypar.h>
+#include"placement/place_db.h"
+
 namespace gigaplace {
 class KaHyPar {
  public:
+  KaHyPar()
+      : context_(kahypar_context_new()),
+        ini_file_name_(),
+        hyperedge_weights_(std::make_unique<kahypar_hyperedge_weight_t[]>(4)),
+        hyperedge_indices_(std::make_unique<size_t[]>(5)),
+        hyperedges_(std::make_unique<kahypar_hyperedge_id_t[]>(12)),
+        imbalance_(0.03),
+        num_vertices_(),
+        num_hyperedges_(),
+        partition_(num_vertices_, -1),
+        k_() {}
   void partition();
-
+ private:
+  kahypar_context_t *context_{};
+  const std::string ini_file_name_{};
+  std::unique_ptr<kahypar_hyperedge_weight_t[]> hyperedge_weights_{};
+  std::unique_ptr<size_t[]> hyperedge_indices_{};
+  std::unique_ptr<kahypar_hyperedge_id_t[]> hyperedges_{};
+  const double imbalance_{};
+  const kahypar_hypernode_id_t num_vertices_{};
+  const kahypar_hyperedge_id_t num_hyperedges_{};
+  std::vector<kahypar_partition_id_t> partition_{};
+  const kahypar_partition_id_t k_{};
+  kahypar_hyperedge_weight_t objective_{};
 };
 }
 #endif //GIGACELL_GIGACELL_PARTITION_KAHYPAR_H_
