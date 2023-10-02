ebc66ed0b1a0d679b924d11d1a2ccd4d191922de
linjingm <1021156572@qq.com>
2023-10-02 16:38:13 +0800

    Fix area calculation

--- gigacell/constraints/area.cc
+++ gigacell/constraints/area.cc
@@ -5,13 +5,19 @@
 #include "area.h"
 #include <limits>
 float gigaplace::Area::computeArea() {
-  float x_max = -std::numeric_limits<float>::max();
-  for (Mos &nmos : place_db_.nmos_list()) {
-    x_max = std::max(x_max, nmos.getGateLoc());
-  }
-
-  for (Mos &pmos : place_db_.pmos_list()) {
-    x_max = std::max(x_max, pmos.getGateLoc());
-  }
-  return x_max + 1;
+//  float x_max = -std::numeric_limits<float>::max();
+//  for (Mos &nmos : place_db_.nmos_list()) {
+//    x_max = std::max(x_max, nmos.getGateLoc());
+//  }
+//
+//  for (Mos &pmos : place_db_.pmos_list()) {
+//    x_max = std::max(x_max, pmos.getGateLoc());
+//  }
+//  return x_max + 1;
+//
+    float x_max = -std::numeric_limits<float>::max();
+    for(Mos &mos : place_db_.mos_list()){
+      x_max = std::max(x_max ,mos.getGateLoc());
+    }
+    return x_max + 1;
 }
