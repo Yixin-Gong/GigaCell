45f2af46c14f8bc7ca30f9b6a55a90701e5b1dd8
linjingm <1021156572@qq.com>
2023-10-03 21:03:32 +0800

    Deleted some unused code; Changed code style

--- gigacell/ops/cluster.cc
+++ gigacell/ops/cluster.cc
@@ -5,22 +5,20 @@
 #include "cluster.h"
 
 void gigaplace::Cluster::creatConfigList() {
-//    std::vector<index> block1=block_;
 
-    for(auto mos1_idx=0;mos1_idx<block_.size();mos1_idx++) {
-        for (auto mos2_idx = mos1_idx+1;mos2_idx<block_.size();mos2_idx++){
-            gigaplace::Operator::creatConfig(block_.at(mos1_idx),block_.at(mos2_idx),config_list_,pl_db_);
-        }
+  for (auto mos1_idx = 0; mos1_idx < block_.size(); mos1_idx++) {
+    for (auto mos2_idx = mos1_idx + 1; mos2_idx < block_.size(); mos2_idx++) {
+      gigaplace::Operator::creatConfig(pl_db_, config_list_,block_.at(mos1_idx), block_.at(mos2_idx));
     }
-    for(auto mos_idx : block_){
-        if(!pl_db_.mos_list().at(mos_idx).getConfigFlag()){
-            gigaplace::Operator::createDummy(mos_idx,pl_db_,config_list_);
+  }
 
-        }
-
+  for (auto mos_idx : block_) {
+    if (!pl_db_.mos_list().at(mos_idx).getConfigFlag()) {
+      gigaplace::Operator::createDummy( pl_db_, config_list_,mos_idx);
     }
+  }
 }
 
 void gigaplace::Cluster::clearConfigList() {
-     std::vector<Configuration>().swap(config_list_);
+  std::vector<Configuration>().swap(config_list_);
 }
