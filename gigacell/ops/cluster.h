137b8373de89292622c460493394130970666d4b
Lkyaaa <2310127354@qq.com>
2023-10-03 17:36:44 +0800

    Implemented creatConfigList and ClearConfigList methods.

--- gigacell/ops/cluster.h
+++ gigacell/ops/cluster.h
@@ -4,21 +4,24 @@
 
 #ifndef GIGACELL_GIGACELL_OPS_CLUSTER_H_
 #define GIGACELL_GIGACELL_OPS_CLUSTER_H_
-#include "placement/place_db.h"
+#include"placement/place_db.h"
 #include "operator.h"
-//namespace gigaplace {
-//
-//class Cluster {
-// public:
-//  Cluster(PlaceDB &pl_db) : pl_db_(pl_db){}
-//  std::vector<Configuration> &config_list() {return config_list_;}
-//
-//  void creatConfigList();
-//
-// private:
-//  std::vector<Configuration> config_list_;
-//  PlaceDB &pl_db_;
-//
-//};
-//}
+namespace gigaplace {
+
+class Cluster {
+ public:
+  Cluster(PlaceDB &pl_db,std::vector<index> &block) : pl_db_(pl_db),block_(block){}
+  std::vector<Configuration> &config_list() {return config_list_;}
+
+  void creatConfigList();
+  void clearConfigList();
+
+
+ private:
+  std::vector<Configuration> config_list_;
+  PlaceDB &pl_db_;
+  std::vector<index> &block_;
+
+};
+}
 #endif //GIGACELL_GIGACELL_OPS_CLUSTER_H_
