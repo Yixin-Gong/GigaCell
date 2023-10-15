ebb08b7a137bedbbc86b74fb7dcc4db3b81e2bbb
linjingm <1021156572@qq.com>
2023-10-15 17:37:35 +0800

    Changed swap ,share method; Implemented adjacentShare , setCoordinates, createNewLayout and splitConfig methods.

--- gigacell/ops/operator.h
+++ gigacell/ops/operator.h
@@ -19,13 +19,15 @@ namespace gigaplace {
 
         static void configFlip(PlaceDB &pl_db, PlaceDB::Configuration &config);
 
-        static void swap(PlaceDB &pl_db, index &index1, index &index2);
+        static void swap(PlaceDB &pl_db, index &pair1, index &pair2);
 
         //TODO:Implement pairing and sharing.@zy and lky
 
         static std::string shouldShare(PlaceDB &pl_db, PlaceDB::Configuration &c1, PlaceDB::Configuration &c2);
 
         static void share(PlaceDB &pl_db, std::vector<PlaceDB::Configuration> &config_list);
+
+        static void adjacentShare(PlaceDB &pl_db, index &pair);
 
         static void addConfig(PlaceDB &pl_db, std::vector<PlaceDB::Configuration> &config_list, index &mos1_idx, index &mos2_idx);
 
@@ -34,6 +36,14 @@ namespace gigaplace {
         static PlaceDB::Configuration creatTempConfig(PlaceDB &pl_db,PlaceDB::Configuration & c1 ,PlaceDB::Configuration & c2);
 
         static void pairSingleMos(PlaceDB &pl_db);
+
+        static void v_configTol_config(gigaplace::PlaceDB &pl_db);
+
+        static void setCoordinates(gigaplace::PlaceDB &pl_db, std::list<PlaceDB::Configuration> &l_config);
+
+        static void createNewLayout(PlaceDB &pl_db, index &pair1, index &pair2);
+
+        static void splitConfig(PlaceDB &pl_db,index &split_pair);
     };
 }
 #endif //GIGACELL_GIGACELL_OPS_OPERATOR_H_
