eed9a275aea3051469bd0a31f7ad430167248d85
eric <3258859094@qq.com>
2023-10-22 21:46:00 +0800

    Added some methods in class GigaPlace

--- gigacell/main/gigaplace.cc
+++ gigacell/main/gigaplace.cc
@@ -3,3 +3,22 @@
 //
 
 #include "gigaplace.h"
+
+float gigaplace::GigaPlace::computeDeltaC(float &new_cost, float &old_cost) {
+  return new_cost - old_cost;
+}
+
+bool gigaplace::GigaPlace::accept(int &delta_c, float &T) {
+  float prob;
+  float r;
+  prob = std::exp((float) (-delta_c) / (float) T);
+
+  r = static_cast <float> (random()) / static_cast <float> (RAND_MAX);
+  if (r < std::min(prob, (float) 1.0))
+    return true;
+  else
+    return false;
+}
+void gigaplace::GigaPlace::SAPlace() {
+
+}
--- gigacell/main/gigaplace.h
+++ gigacell/main/gigaplace.h
@@ -6,9 +6,18 @@
 #define GIGACELL_GIGACELL_MAIN_GIGAPLACE_H_
 
 #include "ops/operator.h"
+#include "placement/place_obj.h"
 namespace gigaplace {
-    class Gigaplace {
+class GigaPlace {
+ public:
+  explicit GigaPlace(PlaceDB &pl_db, PlaceObj &pl_obj) : pl_db_(pl_db), pl_obj_(pl_obj) {};
+  void SAPlace();
+  static float computeDeltaC(float &new_cost, float &old_cost);
+  static bool accept(int &delta_c, float &T);
+ private:
+  PlaceDB &pl_db_;
+  PlaceObj &pl_obj_;
+};
 
-    };
 }
 #endif //GIGACELL_GIGACELL_MAIN_GIGAPLACE_H_
