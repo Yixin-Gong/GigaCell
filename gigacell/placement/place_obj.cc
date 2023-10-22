afc8493d31b21f1c3f2264ea51c56b16f1bae9e0
eric <3258859094@qq.com>
2023-10-22 21:44:02 +0800

    Added place objective function with only non-factor area and netlength constraints

--- gigacell/placement/place_obj.cc
+++ gigacell/placement/place_obj.cc
@@ -3,3 +3,9 @@
 //
 
 #include "place_obj.h"
+
+float gigaplace::PlaceObj::computeCost() {
+  return 50 * area_.computeArea() + 10 * net_length_.netLength();
+}
+
+
--- gigacell/placement/place_obj.h
+++ gigacell/placement/place_obj.h
@@ -4,12 +4,22 @@
 
 #ifndef GIGACELL_GIGACELL_PLACEMENT_PLACE_OBJ_H_
 #define GIGACELL_GIGACELL_PLACEMENT_PLACE_OBJ_H_
+#include "constraints/area.h"
+#include "constraints/netlength.h"
+#include <chrono>
+#include <cmath>
+#include <random>
 
-
+namespace gigaplace {
 class PlaceObj {
  public:
-  PlaceObj()=default;
- private:
-};
+  explicit PlaceObj(Area &area, NetLength &net_length)
+      : area_(area), net_length_(net_length) {}
+  float computeCost();
 
+ private:
+  Area &area_;
+  NetLength &net_length_;
+};
+}
 #endif //GIGACELL_GIGACELL_PLACEMENT_PLACE_OBJ_H_
