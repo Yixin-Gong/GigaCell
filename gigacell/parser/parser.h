a05ecdbae38031e9111a3ea3ee96af3d8abad06e
linjingm <1021156572@qq.com>
2023-10-30 22:29:53 +0800

    Changed parse method to parse all cells in a spice file

--- gigacell/parser/parser.h
+++ gigacell/parser/parser.h
@@ -12,14 +12,14 @@
 namespace gigaplace {
 class Parser {
  public:
-  Parser(DataBase &db, std::string filename) : db_(db), filename_(std::move(filename)), file_() {}
+  Parser(std::string filename, std::vector<DataBase> &total_db) : filename_(std::move(filename)), file_(), total_db_(total_db) {}
 
   void parse();
 
  private:
   std::string filename_;
-  DataBase &db_;
   std::ifstream file_;
+  std::vector<DataBase> &total_db_;
 
   static std::vector<std::string> split_(const std::string &str, char delimiter);
 };
