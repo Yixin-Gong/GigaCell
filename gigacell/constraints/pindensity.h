06f570817fb59821d9ccd440620e57d12082c563
Lkyaaa <2310127354@qq.com>
2023-11-03 16:27:35 +0800

    Implemented getPinScore method;

--- gigacell/constraints/pindensity.h
+++ gigacell/constraints/pindensity.h
@@ -5,16 +5,27 @@
 #ifndef GIGACELL_GIGACELL_CONSTRAINTS_PINDENSITY_H_
 #define GIGACELL_GIGACELL_CONSTRAINTS_PINDENSITY_H_
 
-#include "database/database.h"
+#include "placement/place_db.h"
+#include <algorithm>
+#include <cmath>
+#include <limits>
 namespace  gigaplace {
 
 class PinDensity{
  public:
-  explicit  PinDensity(DataBase &db) : db_(db){}
+  explicit  PinDensity(PlaceDB &pl_db,float &width) : pl_db_(pl_db),width_(width){}
 
-  float pinDensity();
+  float &width(){return width_;}
+
+  static float calStandardDeviation(const std::vector<float> &pin_spacing);
+  float getPinScore();
+  static bool isPinNet(const std::string &netName,const std::vector<std::string> &v_pin_name);
+
+
+
  private:
-  DataBase &db_;
+  PlaceDB &pl_db_;
+  float &width_;
 };
 }
 #endif //GIGACELL_GIGACELL_CONSTRAINTS_PINDENSITY_H_
