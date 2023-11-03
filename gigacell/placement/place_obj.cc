2eb0d95aff0b37e44935cee5569a495e953e20ba
Lkyaaa <2310127354@qq.com>
2023-11-03 21:09:51 +0800

    Implemented methods to calculate score.

--- gigacell/placement/place_obj.cc
+++ gigacell/placement/place_obj.cc
@@ -4,8 +4,73 @@
 
 #include "place_obj.h"
 
-float gigaplace::PlaceObj::computeCost() {
-  return 50 * area_.computeArea() + 10 * net_length_.netLength();
+float gigaplace::PlaceObj::getMinGap() {
+  float u_odd_num;
+  float l_odd_num;
+
+  //make map
+  std::unordered_map<std::string, int32_t> upper_Graph;
+  std::unordered_map<std::string, int32_t> lower_Graph;
+  int32_t unit_degree = 1;
+
+  for (auto &kMos : pl_db_.pmos_list()) {
+    if (upper_Graph.find(kMos.getLeft()) == upper_Graph.end())
+      upper_Graph.emplace(kMos.getLeft(), unit_degree);
+    else
+      upper_Graph.at(kMos.getLeft())++;
+    if (upper_Graph.find(kMos.getRight()) == upper_Graph.end())
+      upper_Graph.emplace(kMos.getRight(), unit_degree);
+    else
+      upper_Graph.at(kMos.getRight())++;
+
+  }
+  for (auto &kMos : pl_db_.nmos_list()) {
+    if (lower_Graph.find(kMos.getLeft()) == lower_Graph.end())
+      lower_Graph.emplace(kMos.getLeft(), unit_degree);
+    else
+      lower_Graph.at(kMos.getLeft())++;
+    if (lower_Graph.find(kMos.getRight()) == lower_Graph.end())
+      lower_Graph.emplace(kMos.getRight(), unit_degree);
+    else
+      lower_Graph.at(kMos.getRight())++;
+  }
+  for (auto &node : upper_Graph) {
+    if (node.second % 2)
+      u_odd_num += 1;
+  }
+  for (auto &node : lower_Graph) {
+    if (node.second % 2)
+      l_odd_num += 1;
+  }
+
+  return std::max((float) 0, (u_odd_num + l_odd_num - 4) / 2);
 }
+void gigaplace::PlaceObj::getCellRefWidth() {
+  auto min_gap = getMinGap();
+  cell_ref_width_ = (min_gap + ref_width_) / 2;
+}
+void gigaplace::PlaceObj::getWidthScore() {
+  Area area(pl_db_);
+  auto width = area.calWidth();
+  ws_ = 40 * (1 - (width - cell_ref_width_) / (cell_ref_width_ + 20));
+}
+void gigaplace::PlaceObj::getNetScore() {
+  NetLength netlength(pl_db_);
+  auto bbox = netlength.calNetLength();
+  auto pin_len = (float) pl_db_.v_pin_list().size();
+  bs_ = std::min((float) 20, 20 * (1 - (bbox - cell_ref_width_ * (pin_len - 1)) / 60));
+}
+void gigaplace::PlaceObj::init() {
+  getCellRefWidth();
+  getWidthScore();
+  getNetScore();
+  getPinScore();
+  score_ = ws_ + bs_+ps_;
+}
+void gigaplace::PlaceObj::getPinScore() {
+  PinDensity pindensity(pl_db_);
+  auto pin_access = pindensity.getPinAccess();
+  ps_ = 10 * (1-pin_access);
 
 
+}
--- gigacell/placement/place_obj.h
+++ gigacell/placement/place_obj.h
@@ -2,24 +2,46 @@
 // Created by eric on 23-9-21.
 //
 
+
 #ifndef GIGACELL_GIGACELL_PLACEMENT_PLACE_OBJ_H_
 #define GIGACELL_GIGACELL_PLACEMENT_PLACE_OBJ_H_
 #include "constraints/area.h"
 #include "constraints/netlength.h"
+#include "constraints/pindensity.h"
 #include <chrono>
 #include <cmath>
 #include <random>
+#include "placement/place_db.h"
+#include <limits>
 
 namespace gigaplace {
 class PlaceObj {
  public:
-  explicit PlaceObj(Area &area, NetLength &net_length)
-      : area_(area), net_length_(net_length) {}
-  float computeCost();
+  explicit PlaceObj(PlaceDB &pl_db, float &ref_width)
+      : pl_db_(pl_db), ref_width_(ref_width) {
+    init();
+
+  }
+  float getMinGap();
+  void getCellRefWidth();
+  void getWidthScore();
+  void getNetScore();
+  void getPinScore();
+  float &get_score() { return score_; };
+
+  void init();
 
  private:
-  Area &area_;
-  NetLength &net_length_;
+  PlaceDB &pl_db_;
+  float &ref_width_;
+
+  float cell_ref_width_{};
+
+  float ws_{};
+  float bs_{};
+  float ps_{};
+
+  float score_{};
 };
 }
 #endif //GIGACELL_GIGACELL_PLACEMENT_PLACE_OBJ_H_
