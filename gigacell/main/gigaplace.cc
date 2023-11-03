ca6925e7c123bf9123972d244ebae20b52409453
eric <3258859094@qq.com>
2023-11-03 21:06:13 +0800

    Implemented SAPlace method

--- gigacell/main/gigaplace.cc
+++ gigacell/main/gigaplace.cc
@@ -3,15 +3,15 @@
 //
 
 #include "gigaplace.h"
-
+#include <random>
 float gigaplace::GigaPlace::computeDeltaC(float &new_cost, float &old_cost) {
   return new_cost - old_cost;
 }
 
-bool gigaplace::GigaPlace::accept(int &delta_c, float &T) {
+bool gigaplace::GigaPlace::accept(float &delta_c, float T) {
   float prob;
   float r;
-  prob = std::exp((float) (-delta_c) / (float) T);
+  prob = std::exp(-delta_c / T);
 
   r = static_cast <float> (random()) / static_cast <float> (RAND_MAX);
   if (r < std::min(prob, (float) 1.0))
@@ -19,6 +19,35 @@ bool gigaplace::GigaPlace::accept(int &delta_c, float &T) {
   else
     return false;
 }
-void gigaplace::GigaPlace::SAPlace() {
 
+std::pair<uint16_t, uint16_t> gigaplace::GigaPlace::generate2Num(PlaceDB &place_db, uint16_t pair_num) {
+  std::pair<uint16_t, uint16_t> pair{};
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<> distrib(0, pair_num);
+
+  pair.first = distrib(gen);
+  pair.second = distrib(gen);
+  while (pair.first == pair.second)
+    pair.second = distrib(gen);
+  return pair;
+}
+
+void gigaplace::GigaPlace::SAPlace(uint16_t pair_num) {
+  while (T > 0.001) {
+    Operator::setCoordinates(pl_db_, pl_db_.l_config());
+    auto config_list = pl_db_.l_config();
+    auto old_cost = pl_obj_.computeCost();
+    auto pair = generate2Num(pl_db_, pair_num);
+
+    Operator::createNewLayout(pl_db_, pair.first, pair.second);
+    Operator::setCoordinates(pl_db_, pl_db_.l_config());
+    auto new_cost = pl_obj_.computeCost();
+
+    auto delta_C = computeDeltaC(new_cost, old_cost);
+    if (!accept(delta_C, T)) {
+      pl_db_.l_config() = config_list;
+    }
+  }
+  T *= 0.9;
 }
--- gigacell/main/gigaplace.h
+++ gigacell/main/gigaplace.h
@@ -10,13 +10,15 @@
 namespace gigaplace {
 class GigaPlace {
  public:
-  explicit GigaPlace(PlaceDB &pl_db, PlaceObj &pl_obj) : pl_db_(pl_db), pl_obj_(pl_obj) {};
-  void SAPlace();
+  explicit GigaPlace(PlaceDB &pl_db, PlaceObj &pl_obj, float T) : pl_db_(pl_db), pl_obj_(pl_obj),T(10000) {};
+  void SAPlace(uint16_t pair_num);
   static float computeDeltaC(float &new_cost, float &old_cost);
-  static bool accept(int &delta_c, float &T);
+  static bool accept(float &delta_c, float T);
+  static std::pair<uint16_t, uint16_t> generate2Num(PlaceDB &place_db, uint16_t pair_num);
  private:
   PlaceDB &pl_db_;
   PlaceObj &pl_obj_;
+  float T;
 };
 
 }
