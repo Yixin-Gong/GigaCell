47792187dd4e87b7468b27d1c7937d8555c45e67
linjingm <1021156572@qq.com>
2023-11-05 17:58:17 +0800

    Implement writer method.

--- gigacell/writer/writer.h
+++ gigacell/writer/writer.h
@@ -4,9 +4,15 @@
 
 #ifndef GIGACELL_GIGACELL_WRITER_WRITER_H_
 #define GIGACELL_GIGACELL_WRITER_WRITER_H_
-
+#include "placement/place_db.h"
+namespace gigaplace{
 class writer {
+ public:
+  writer() = default;
+  static void exporter(PlaceDB &pl_db);
 
 };
+
+}
 
 #endif //GIGACELL_GIGACELL_WRITER_WRITER_H_
