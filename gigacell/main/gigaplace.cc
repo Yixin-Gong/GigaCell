5d005952d1469f4ba900d63b6ed476dcd62df933
eric <3258859094@qq.com>
2023-11-07 19:51:41 +0800

    Added get score method

--- gigacell/main/gigaplace.cc
+++ gigacell/main/gigaplace.cc
@@ -35,7 +35,7 @@ std::pair<uint16_t, uint16_t> gigaplace::GigaPlace::generate2Num(PlaceDB &place_
 
 void gigaplace::GigaPlace::SAPlace(uint16_t pair_num) {
 //  auto iter = 0;
-  while (T > 0.0001) {
+  while (T_ > 0.0001) {
     Operator::setCoordinates(pl_db_, pl_db_.l_config());
     auto place_obj = new gigaplace::PlaceObj(pl_db_, ref_width_);
 
@@ -44,6 +44,7 @@ void gigaplace::GigaPlace::SAPlace(uint16_t pair_num) {
     auto nets = pl_db_.nets();
 
     auto old_cost = -place_obj->get_score();
+    score_ = old_cost;
     std::cout << "old_cost" << old_cost << std::endl;
     auto pair = generate2Num(pl_db_, pair_num);
     delete place_obj;
@@ -57,13 +58,11 @@ void gigaplace::GigaPlace::SAPlace(uint16_t pair_num) {
     delete place_obj;
 
     auto delta_C = computeDeltaC(new_cost, old_cost);
-    if (!accept(delta_C, T)) {
+    if (!accept(delta_C, T_)) {
       pl_db_.l_config() = config_list;
       pl_db_.mos_list() = mos_list;
       pl_db_.nets() = nets;
     }
-    T *= 0.99;
-//    iter++;
-//    std::cout<<iter<<std::endl;
+    T_ *= 0.99;
   }
 }
