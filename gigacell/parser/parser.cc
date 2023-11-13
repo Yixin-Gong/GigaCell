7d6f09648d11ea4ea3778ce0750218cfc947c9d8
Lkyaaa <2310127354@qq.com>
2023-11-13 19:53:02 +0800

    Fixed parse method.

--- gigacell/parser/parser.cc
+++ gigacell/parser/parser.cc
@@ -1,3 +1,167 @@
+////
+//// Created by eric on 23-9-15.
+////
+//
+//#include "parser/parser.h"
+//#include <sstream>
+//
+//void gigaplace::Parser::parse() {
+//  file_.open(filename_);
+//  if (!file_.is_open()) {
+//    std::cerr << "Failed to open file" << filename_ << std::endl;
+//    return;
+//  }
+//  Mos mos;
+//  DataBase::Pin pin;
+//  DataBase cell_db;
+//  std::string subStr;
+//  int32_t pmos_idx = 0;
+//  int32_t nmos_idx = 0;
+//  std::string line;
+//
+//
+//  while (std::getline(file_, line)) {
+//    std::vector<std::string> tokens = split_(line, ' ');
+//    if (tokens[0] == ".SUBCKT") {
+//      cell_db.cell_name() = tokens[1];
+//      for (int32_t i = 2; i < tokens.size() - 2; i++) {
+//        pin.pinName = tokens[i];
+//        cell_db.v_pin_list().push_back(pin);
+//      }
+//      continue;
+//    }
+//    if (tokens[0].at(0) == '.' && tokens[0].at(1) == 'E') {
+//      total_db_.push_back(cell_db);
+//      pmos_idx = 0;
+//      nmos_idx = 0;
+//      //   cell_db.clearDB();
+//      std::vector <index>().swap(cell_db.nmos_ids());
+//      std::vector <index>().swap(cell_db.pmos_ids());
+//      std::vector <Mos>().swap(cell_db.nmos_list());
+//      std::vector <Mos>().swap(cell_db.pmos_list());
+//      std::vector <DataBase::Pin>().swap(cell_db.v_pin_list());
+//      cell_db.cell_name().clear();
+//      continue;
+//    }
+//    std::string mos_initial;
+//    mos_initial = tokens[5].substr(0, 1);
+//    if (mos_initial == "n") {
+//      std::string w_initial;
+//      w_initial = tokens[6].substr(0, 1);
+//      if (w_initial == "W" || w_initial == "w") {
+//        std::string num_str;
+//        for (char c : tokens[6]) {
+//          if (isdigit(c) || c == '.')
+//            num_str += c;
+//        }
+//        std::string w_end;
+//        w_end = tokens[6].substr(tokens[6].size() - 1, 1);
+//        float width;
+//        std::istringstream iss(num_str);
+//        iss >> width;
+//        if (w_end == "u") {
+//          mos.getWidth() = width * 1000;
+//        } else {
+//          mos.getWidth() = width;
+//        }
+//      } else {
+//        std::string num_str;
+//        for (char c : tokens[7]) {
+//          if (isdigit(c) || c == '.')
+//            num_str += c;
+//        }
+//
+//        std::string w_end;
+//        if (tokens[7].substr(tokens[7].size() - 1) == "\r")
+//          w_end = tokens[7].substr(tokens[7].size() - 2, 1);
+//        else
+//          w_end = tokens[7].substr(tokens[7].size() - 1, 1);
+//
+//        float width;
+//        std::istringstream iss(num_str);
+//        iss >> width;
+//        if (w_end == "u") {
+//          mos.getWidth() = width * 1000;
+//        } else {
+//          mos.getWidth() = width;
+//        }
+//      }
+//      subStr = tokens[0].substr(1);
+//      mos.getMosName() = subStr;
+//      mos.getRight() = tokens[3];
+//      mos.getGate() = tokens[2];
+//      mos.getLeft() = tokens[1];
+//      mos.getType() = 0;
+//      //mos.getWidth()=
+//      cell_db.nmos_list().push_back(mos);
+//      cell_db.nmos_ids().push_back(nmos_idx);
+//      nmos_idx++;
+//    } else {
+//      //pmos
+//      std::string w_initial;
+//      w_initial = tokens[6].substr(0, 1);
+//      if (w_initial == "W" || w_initial == "w") {
+//        std::string num_str;
+//        for (char c : tokens[6]) {
+//          if (isdigit(c) || c == '.')
+//            num_str += c;
+//        }
+//        std::string w_end;
+//        w_end = tokens[6].substr(tokens[6].size() - 1, 1);
+//        float width;
+//        std::istringstream iss(num_str);
+//        iss >> width;
+//        if (w_end == "u") {
+//          mos.getWidth() = width * 1000;
+//        } else {
+//          mos.getWidth() = width;
+//        }
+//      } else {
+//        std::string num_str;
+//        for (char c : tokens[7]) {
+//          if (isdigit(c) || c == '.')
+//            num_str += c;
+//        }
+//
+//        std::string w_end;
+//        if (tokens[7].at(tokens[7].size()-1) == '\r')
+//          w_end = tokens[7].substr(tokens[7].size() - 2, 1);
+//        else
+//          w_end = tokens[7].substr(tokens[7].size() - 1, 1);
+//
+//        float width;
+//        std::istringstream iss(num_str);
+//        iss >> width;
+//        if (w_end == "u") {
+//          mos.getWidth() = width * 1000;
+//        } else {
+//          mos.getWidth() = width;
+//        }
+//      }
+//      subStr = tokens[0].substr(1);
+//      mos.getMosName() = subStr;
+//      mos.getRight() = tokens[3];
+//      mos.getGate() = tokens[2];
+//      mos.getLeft() = tokens[1];
+//      mos.getType() = 1;
+//      //mos.getWidth()=
+//      cell_db.pmos_list().push_back(mos);
+//      cell_db.pmos_ids().push_back(pmos_idx);
+//      pmos_idx++;
+//    }
+//
+//  }
+//  file_.close();
+//}
+//std::vector<std::string> gigaplace::Parser::split_(const std::string &str, char delimiter) {
+//  std::vector<std::string> tokens;
+//  std::string token;
+//  std::istringstream tokenStream(str);
+//  while (getline(tokenStream, token, delimiter)) {
+//    tokens.push_back(token);
+//  }
+//  return tokens;
+//}
 //
 // Created by eric on 23-9-15.
 //
@@ -16,18 +180,16 @@ void gigaplace::Parser::parse() {
   int32_t pmos_idx = 0;
   int32_t nmos_idx = 0;
   std::string line;
-  index flag = 0;
+  index flag_name = 0;
 
   while (std::getline(file_, line)) {
     std::vector<std::string> tokens = split_(line, ' ');
-
     if (tokens[0] == ".SUBCKT") {
       if (tokens[1] != cell_name_)
         continue;
-      flag = 1;
+      flag_name = 1;
       db_.cell_name() = tokens[1];
-//      pin.pinName = tokens[i].substr(0, tokens[i].size() - 1);
-//      db_.v_pin_list().push_back(pin);
+
       auto size = tokens.size();
       if(tokens[size -1] == "\r"){
         for(int32_t i = 2; i < tokens.size()-1; i++){
@@ -36,11 +198,11 @@ void gigaplace::Parser::parse() {
           db_.v_pin_list().push_back(tokens[i]);
         }
       }else{
-        for(int32_t i =2; i < tokens.size() ; i++){
+        for(int32_t i =2; i < tokens.size(); i++){
           if(tokens[i] == "VDD" || tokens[i] == "VSS" || tokens[i] == "VDD\r" || tokens[i] == "VSS\r")
             continue;
           if(i == tokens.size() - 1){
-            db_.v_pin_list().push_back(tokens[i].substr(0, tokens[i].size() - 1));
+            db_.v_pin_list().push_back(tokens[i].substr(0,tokens[i].size() - 1));
             continue;
           }
           db_.v_pin_list().push_back(tokens[i]);
@@ -48,121 +210,127 @@ void gigaplace::Parser::parse() {
       }
       continue;
     }
-    if(flag == 0)
-      continue;
-    if (flag == 1 && tokens[0].at(0) == '.' && tokens[0].at(1) == 'E') {
-      break;
-    }
-    std::string mos_initial;
-    mos_initial = tokens[5].substr(0, 1);
-    if (mos_initial == "n") {
-      std::string w_initial;
-      w_initial = tokens[6].substr(0, 1);
-      if (w_initial == "W" || w_initial == "w") {
-        std::string num_str;
-        for (char c : tokens[6]) {
-          if (isdigit(c) || c == '.')
-            num_str += c;
-        }
-        std::string w_end;
-        w_end = tokens[6].substr(tokens[6].size() - 1, 1);
-        float width;
-        std::istringstream iss(num_str);
-        iss >> width;
-        if (w_end == "u") {
-          mos.getWidth() = width * 1000;
-        } else {
-          mos.getWidth() = width;
-        }
-      } else {
-        std::string num_str;
-        for (char c : tokens[7]) {
-          if (isdigit(c) || c == '.')
-            num_str += c;
-        }
 
-        std::string w_end;
-        if (tokens[7].substr(tokens[7].size() - 1) == "\r")
-          w_end = tokens[7].substr(tokens[7].size() - 2, 1);
-        else
-          w_end = tokens[7].substr(tokens[7].size() - 1, 1);
+      if (tokens[0].at(0) == '.' && tokens[0].at(1) == 'E' && flag_name == 1) {
+        break;
+      }
+      if (tokens[0].at(0) == '.' && tokens[0].at(1) == 'E') {
+        continue;
+      }
+      // std::string first = tokens[0].substr(0,1);
+      if (db_.cell_name() == cell_name_) {
+        std::string mos_initial;
+        mos_initial = tokens[5].substr(0, 1);
+        if (mos_initial == "n") {
+          std::string w_initial;
+          w_initial = tokens[6].substr(0, 1);
+          if (w_initial == "W" || w_initial == "w") {
+            std::string num_str;
+            for (char c : tokens[6]) {
+              if (isdigit(c) || c == '.')
+                num_str += c;
+            }
+            std::string w_end;
+            w_end = tokens[6].substr(tokens[6].size() - 1, 1);
+            float width;
+            std::istringstream iss(num_str);
+            iss >> width;
+            if (w_end == "u") {
+              mos.getWidth() = width * 1000;
+            } else {
+              mos.getWidth() = width;
+            }
+          } else {
+            std::string num_str;
+            for (char c : tokens[7]) {
+              if (isdigit(c) || c == '.')
+                num_str += c;
+            }
 
-        float width;
-        std::istringstream iss(num_str);
-        iss >> width;
-        if (w_end == "u") {
-          mos.getWidth() = width * 1000;
+            std::string w_end;
+            if (tokens[7].substr(tokens[7].size() - 1) == "\r")
+              w_end = tokens[7].substr(tokens[7].size() - 2, 1);
+            else
+              w_end = tokens[7].substr(tokens[7].size() - 1, 1);
+
+            float width;
+            std::istringstream iss(num_str);
+            iss >> width;
+            if (w_end == "u") {
+              mos.getWidth() = width * 1000;
+            } else {
+              mos.getWidth() = width;
+            }
+          }
+          subStr = tokens[0].substr(1);
+          mos.getMosName() = subStr;
+          mos.getRight() = tokens[3];
+          mos.getGate() = tokens[2];
+          mos.getLeft() = tokens[1];
+          mos.getType() = 0;
+          //mos.getWidth()=
+          db_.nmos_list().push_back(mos);
+          db_.nmos_ids().push_back(nmos_idx);
+          nmos_idx++;
         } else {
-          mos.getWidth() = width;
+          //pmos
+          std::string w_initial;
+          w_initial = tokens[6].substr(0, 1);
+          if (w_initial == "W" || w_initial == "w") {
+            std::string num_str;
+            for (char c : tokens[6]) {
+              if (isdigit(c) || c == '.')
+                num_str += c;
+            }
+            std::string w_end;
+            w_end = tokens[6].substr(tokens[6].size() - 1, 1);
+            float width;
+            std::istringstream iss(num_str);
+            iss >> width;
+            if (w_end == "u") {
+              mos.getWidth() = width * 1000;
+            } else {
+              mos.getWidth() = width;
+            }
+          } else {
+            std::string num_str;
+            for (char c : tokens[7]) {
+              if (isdigit(c) || c == '.')
+                num_str += c;
+            }
+
+            std::string w_end;
+            if (tokens[7].at(tokens[7].size() - 1) == '\r')
+              w_end = tokens[7].substr(tokens[7].size() - 2, 1);
+            else
+              w_end = tokens[7].substr(tokens[7].size() - 1, 1);
+
+            float width;
+            std::istringstream iss(num_str);
+            iss >> width;
+            if (w_end == "u") {
+              mos.getWidth() = width * 1000;
+            } else {
+              mos.getWidth() = width;
+            }
+          }
+          subStr = tokens[0].substr(1);
+          mos.getMosName() = subStr;
+          mos.getRight() = tokens[3];
+          mos.getGate() = tokens[2];
+          mos.getLeft() = tokens[1];
+          mos.getType() = 1;
+          //mos.getWidth()=
+          db_.pmos_list().push_back(mos);
+          db_.pmos_ids().push_back(pmos_idx);
+          pmos_idx++;
         }
       }
-      subStr = tokens[0].substr(1);
-      mos.getMosName() = subStr;
-      mos.getRight() = tokens[3];
-      mos.getGate() = tokens[2];
-      mos.getLeft() = tokens[1];
-      mos.getType() = 0;
-      //mos.getWidth()=
-      db_.nmos_list().push_back(mos);
-      db_.nmos_ids().push_back(nmos_idx);
-      nmos_idx++;
-    } else {
-      //pmos
-      std::string w_initial;
-      w_initial = tokens[6].substr(0, 1);
-      if (w_initial == "W" || w_initial == "w") {
-        std::string num_str;
-        for (char c : tokens[6]) {
-          if (isdigit(c) || c == '.')
-            num_str += c;
-        }
-        std::string w_end;
-        w_end = tokens[6].substr(tokens[6].size() - 1, 1);
-        float width;
-        std::istringstream iss(num_str);
-        iss >> width;
-        if (w_end == "u") {
-          mos.getWidth() = width * 1000;
-        } else {
-          mos.getWidth() = width;
-        }
-      } else {
-        std::string num_str;
-        for (char c : tokens[7]) {
-          if (isdigit(c) || c == '.')
-            num_str += c;
-        }
-
-        std::string w_end;
-        if (tokens[7].at(tokens[7].size() - 1) == '\r')
-          w_end = tokens[7].substr(tokens[7].size() - 2, 1);
-        else
-          w_end = tokens[7].substr(tokens[7].size() - 1, 1);
-
-        float width;
-        std::istringstream iss(num_str);
-        iss >> width;
-        if (w_end == "u") {
-          mos.getWidth() = width * 1000;
-        } else {
-          mos.getWidth() = width;
-        }
-      }
-      subStr = tokens[0].substr(1);
-      mos.getMosName() = subStr;
-      mos.getRight() = tokens[3];
-      mos.getGate() = tokens[2];
-      mos.getLeft() = tokens[1];
-      mos.getType() = 1;
-      //mos.getWidth()=
-      db_.pmos_list().push_back(mos);
-      db_.pmos_ids().push_back(pmos_idx);
-      pmos_idx++;
     }
-
+    file_.close();
   }
-  file_.close();
-}
+
+
 std::vector<std::string> gigaplace::Parser::split_(const std::string &str, char delimiter) {
   std::vector<std::string> tokens;
   std::string token;
--- gigacell/parser/parser.h
+++ gigacell/parser/parser.h
@@ -1,3 +1,31 @@
+////
+//// Created by eric on 23-9-15.
+////
+//
+//#ifndef GIGACELL_GIGACELL_PARSER_PARSER_H_
+//#define GIGACELL_GIGACELL_PARSER_PARSER_H_
+//
+//#include <utility>
+//#include "database/database.h"
+//#include <fstream>
+//
+//namespace gigaplace {
+//class Parser {
+// public:
+//  Parser(std::string filename, std::vector<DataBase> &total_db) : filename_(std::move(filename)), file_(), total_db_(total_db) {}
+//
+//  void parse();
+//
+// private:
+//  std::string filename_;
+//  std::ifstream file_;
+//  std::vector<DataBase> &total_db_;
+//
+//  static std::vector<std::string> split_(const std::string &str, char delimiter);
+//};
+//}
+//#endif //GIGACELL_GIGACELL_PARSER_PARSER_H_
+
 //
 // Created by eric on 23-9-15.
 //
