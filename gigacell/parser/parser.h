629e02b8bea47b0851655c51c57d9fbf54f55b59
Eric <3258859094@qq.com>
2023-11-15 20:21:33 +0800

    Removed unnecessary comments

--- gigacell/parser/parser.h
+++ gigacell/parser/parser.h
@@ -1,31 +1,3 @@
-////
-//// Created by eric on 23-9-15.
-////
-//
-//#ifndef GIGACELL_GIGACELL_PARSER_PARSER_H_
-//#define GIGACELL_GIGACELL_PARSER_PARSER_H_
-//
-//#include <utility>
-//#include "database/database.h"
-//#include <fstream>
-//
-//namespace gigaplace {
-//class Parser {
-// public:
-//  Parser(std::string filename, std::vector<DataBase> &total_db) : filename_(std::move(filename)), file_(), total_db_(total_db) {}
-//
-//  void parse();
-//
-// private:
-//  std::string filename_;
-//  std::ifstream file_;
-//  std::vector<DataBase> &total_db_;
-//
-//  static std::vector<std::string> split_(const std::string &str, char delimiter);
-//};
-//}
-//#endif //GIGACELL_GIGACELL_PARSER_PARSER_H_
-
 //
 // Created by eric on 23-9-15.
 //
@@ -40,9 +12,8 @@
 namespace gigaplace {
 class Parser {
  public:
-  Parser(std::string filename, DataBase &db , std::string cellname) : filename_(std::move(filename)), file_(), db_(db),
-                                                                                        cell_name_(std::move(cellname)) {}
-
+  Parser(std::string filename, DataBase &db, std::string cellname) : filename_(std::move(filename)), file_(), db_(db),
+                                                                     cell_name_(std::move(cellname)) {}
   void parse();
 
  private:
